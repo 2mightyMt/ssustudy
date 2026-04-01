416c82feec45ee19c5dc4cc1244aedee508999f6
HanMin <gksals0641@gmail.com>
2026-04-01 18:23:06 +0900

    [add]baekjoon

---
+++ Hanmin/baekJoon/3_반복문/7__11021.c
@@ -0,0 +1,13 @@
+//오후1:16~오후1:19 / 3분 소요
+
+#include <stdio.h>
+
+int main() {
+    int t, a, b;
+    scanf("%d", &t);
+    for(int i = 1; i <= t; i++){
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d\n", i, a + b);
+    }
+    return 0;
+}
